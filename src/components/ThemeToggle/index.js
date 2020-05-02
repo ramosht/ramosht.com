d6f2e7efa5d26640d558fd202953c2aad8336374
Guilherme Ramos <guilhermeht.ramos@gmail.com>
2020-05-02 00:20:44 -0300

    feat: função toggle do tema

--- src/components/ThemeToggle/index.js
+++ src/components/ThemeToggle/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './styled';
 
 const ThemeToggle = () => {
+  const [ theme, setTheme ] = useState('light');
+
+  useEffect(() => {
+    setTheme(window.__theme);
+    window.__onThemeChange = () => setTheme(window.__theme);
+  }, [])
+
+  useEffect(() => {
+    window.__setPreferredTheme(theme);
+  }, [theme])
+
   return (
-    <S.ThemeToggleWrapper></S.ThemeToggleWrapper>
+    <S.ThemeToggleWrapper 
+      onClick={() => {
+        theme === 'light' ? setTheme('dark') : setTheme('light');
+      }} 
+    >
+      <S.ThemeToggleIcon>☀️</S.ThemeToggleIcon>
+      <S.ThemeToggleBullet selected={ theme } />
+      <S.ThemeToggleIcon>🌙</S.ThemeToggleIcon>
+    </S.ThemeToggleWrapper>
   )
 }
 
--- src/components/ThemeToggle/styled.js
+++ src/components/ThemeToggle/styled.js
@@ -1,8 +1,36 @@
 import styled from 'styled-components';
 
 export const ThemeToggleWrapper = styled.button`
-  width: 4rem;
+  width: 3rem;
   height: 1.65rem;
-  background: var(--postBackground);
+  background: #261a26;
   border-radius: 1.65rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  
+  &:active,
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
+
+export const ThemeToggleBullet = styled.div`
+  height: 1rem;
+  width: 1rem;
+  background: #fff;
+  border-radius: 50%;
+  position: absolute;
+  top: 50%;
+  transition: left .2s;
+  left: ${ p => p.selected == 'light' ? '0.75rem' : 'calc(100% - 0.75rem)' };
+  transform: translate(-50%, -50%);
+  
+`;
+
+export const ThemeToggleIcon = styled.div``;
