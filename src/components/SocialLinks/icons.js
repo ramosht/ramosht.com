880e4dbba48bbe919c0263585dc8381950dd6f50
Guilherme Ramos <guilhermeht.ramos@gmail.com>
2020-05-02 14:43:51 -0300

    feat: case Icon

--- src/components/SocialLinks/icons.js
+++ src/components/SocialLinks/icons.js
@@ -1,6 +1,6 @@
-import { Github } from '@styled-icons/boxicons-logos/Github';
-import { Twitter } from '@styled-icons/boxicons-logos/Twitter';
-import { Instagram } from '@styled-icons/boxicons-logos/Instagram';
+import { Github } from '@styled-icons/boxicons-logos/Github'
+import { Twitter } from '@styled-icons/boxicons-logos/Twitter'
+import { Instagram } from '@styled-icons/boxicons-logos/Instagram'
 
 const Icons = {
     Github,
